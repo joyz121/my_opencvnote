76548785e7536cbbb5bbd385d4c547114d77b2ec
joe <907612252@qq.com>
2020-02-17 11:59:23 +0800

    添加了Trackbar

--- 空间滤波/demo5.cpp
+++ 空间滤波/demo5.cpp
@@ -4,26 +4,67 @@
 using namespace cv;
 using namespace std;
 
+//全局变量声明
+Mat srcImage, dstImage,dstImage2,dstImage3,dstImage4;
+int g_nMeanBulrValue = 3;
+int g_nGaussianBulrValue = 3;
+int g_nMedianBlurValue = 5;
+int g_nBilateralFilterValue = 6;
+
+//回调函数声明
+static void on_Meanblur(int,void*);
+static void on_GaussianBulr(int, void*);
+static void on_MedianBlur(int, void*);
+static void on_BilateralFilter(int, void*);
+
 int main()
 {
-	Mat src, dst;
-	src = imread("lena.jpg");
-	if (src.empty())
+	srcImage = imread("lena.jpg");
+	if (srcImage.empty())
 	{
 		cout << "could not load image" << endl;
 	}
-	//均值滤波 将窗口内对应图像所有像素的平均值作为模板中心点的像素值
-	blur(src, dst, Size(4,4 ), Point(-1, -1));
-	imshow("均值滤波",dst);
-	//高斯滤波 高斯分布模型赋权重
-	GaussianBlur(src, dst, Size(5, 5), 5,5);
-	imshow("高斯滤波", dst);
-	//中值滤波
-	medianBlur(src, dst, 5);
-	imshow("中值滤波",dst);
-	//双边滤波
-	bilateralFilter(src,dst,5,100,3);
-	imshow("双边滤波",dst);
+	namedWindow("均值滤波", 1);
+	createTrackbar("内核值","均值滤波",&g_nMeanBulrValue,40,on_Meanblur);
+	on_Meanblur(g_nMeanBulrValue,0);
+	
+	namedWindow("高斯滤波",1);
+	createTrackbar("内核值", "高斯滤波", &g_nGaussianBulrValue, 40, on_GaussianBulr);
+	on_GaussianBulr(g_nGaussianBulrValue,0);
+
+	namedWindow("中值滤波", 1);
+	createTrackbar("参考值", "中值滤波", &g_nMedianBlurValue, 40, on_MedianBlur);
+	on_MedianBlur(g_nMedianBlurValue,0);
+
+	namedWindow("双边滤波",1);
+	createTrackbar("参考值", "双边滤波", &g_nBilateralFilterValue, 50, on_BilateralFilter);
+	on_BilateralFilter(g_nBilateralFilterValue,0);
 	waitKey(0);
 	return 0;
+}
+
+
+
+//====================线性滤波======================//
+static void on_Meanblur(int, void*)
+{
+	blur(srcImage, dstImage, Size(g_nMeanBulrValue + 1, g_nMeanBulrValue + 1), Point(-1, -1));
+	imshow("均值滤波", dstImage);
+}
+static void on_GaussianBulr(int, void*)
+{
+	GaussianBlur(srcImage, dstImage2, Size(g_nGaussianBulrValue * 2 + 1, g_nGaussianBulrValue * 2 + 1), 0, 0);
+	imshow("高斯滤波", dstImage2);
+}
+//====================非线性滤波======================//
+static void on_MedianBlur(int, void*)
+{
+	//第三个参数必须是大于1的奇数
+	medianBlur(srcImage, dstImage3, g_nMedianBlurValue * 2 + 1);
+	imshow("中值滤波", dstImage3);
+}
+static void on_BilateralFilter(int, void*)
+{
+	bilateralFilter(srcImage, dstImage4, g_nBilateralFilterValue, g_nBilateralFilterValue * 2, g_nBilateralFilterValue / 2);
+	imshow("双边滤波", dstImage4);
 }
